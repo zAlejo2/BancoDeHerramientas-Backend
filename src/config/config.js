6608018129678cf01b8e6a82d13d4117ec2fbc02
Jose Alejandro Palacio Giraldo <palaciogiraldojosealejandro@gmail.com>
2025-07-09 09:51:57 -0500

    fix: configuración para conectar correctamente con Railway desde Render

--- src/config/config.js
+++ src/config/config.js
@@ -3,25 +3,25 @@ dotenv.config();
 
 const config = {
     app: {
-        port: process.env.PORT
+        port: Number(process.env.PORT) || 10000
     }, 
     jwt: {
         secret: process.env.JWT_SECRET,
         secretnewcontrasena: process.env.JWT_SECRET_RESTABLECER_CONTRASENA
     },
     mysql: {
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,  
-        database: process.env.MYSQL_DATABASE, 
-        port: process.env.MYSQL_PORT || 3306
+        host: process.env.MYSQL_HOST || 'switchyard.proxy.rlwy.net', // <- Usa el externo
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        port: Number(process.env.MYSQL_PORT) || 3306
     },
     email: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
         host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT
+        port: Number(process.env.EMAIL_PORT) || 587
     }
 };
 
-export default config;
+export default config;
