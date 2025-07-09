a6973ad4e5c8b9b98111c9c283933ef41e7392ce
Jose Alejandro Palacio Giraldo <palaciogiraldojosealejandro@gmail.com>
2025-07-09 09:04:06 -0500

    fix: eliminar app.listen para evitar EADDRINUSE en Render

--- index.js
+++ index.js
@@ -2,8 +2,5 @@
 import 'dotenv/config';
 import app from './src/app.js';
 
-const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
-  console.log('Servidor escuchando en el puerto', PORT);
-});
+export default app;
